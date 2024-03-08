8c81f51be15c36cdc1dd270423b3a58c28d1a8f4
wilsondagah <wdagah01@gmail.com>
2024-03-08 00:45:44 +0000

    Working with SocketIO

--- frontend/views/chatpage/ChatPage.tsx
+++ frontend/views/chatpage/ChatPage.tsx
@@ -5,112 +5,125 @@ import type { MessageListItem } from "@vaadin/message-list";
 import Logo from "../../assets/logo.png";
 import Navbar from "../../components/Navbar/Navbar";
 import "../../themes/insuranceapp/styles.css";
-
+import Message from "Frontend/generated/com/example/application/ChatEndpoint/Message";
+import { ChatEndpoint } from "Frontend/generated/endpoints";
 
 // import { getPeople } from 'Frontend/demo/domain/DataService';
 
 export default function ChatPage() {
-  const [items, setItems] = useState<MessageListItem[]>([]);
+  function getPeople(arg0: { count: number }) {
+    throw new Error("Function not implemented.");
+  }
 
-  //   useEffect(() => {
-  //     getPeople({ count: 1 }).then(({ people }) => {
-  //       const person = people[0];
-  //       setItems([
-  //         {
-  //           text: 'Nature does not hurry, yet everything gets accomplished.',
-  //           time: 'yesterday',
-  //           userName: 'Matt Mambo',
-  //           userColorIndex: 1,
-  //         },
-  //         {
-  //           text: 'Using your talent, hobby or profession in a way that makes you contribute with something good to this world is truly the way to go.',
-  //           time: 'right now',
-  //           userName: 'Linsey Listy',
-  //           userColorIndex: 2,
-  //           userImg: person.pictureUrl,
-  //         },
-  //       ]);
-  //     });
-  //   }, []);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const [items, setItems] = useState<MessageListItem[]>([]);
+  const [userName, setUserName] = useState<String>("");
+
+  // useEffect(() => {
+  //   getPeople({ count: 1 }).then(({ people }) => {
+  //     const person = people[0];
+  //     setItems([
+  //       {
+  //         text: "Nature does not hurry, yet everything gets accomplished.",
+  //         time: "yesterday",
+  //         userName: "Matt Mambo",
+  //         userColorIndex: 1,
+  //       },
+  //       {
+  //         text: "Using your talent, hobby or profession in a way that makes you contribute with something good to this world is truly the way to go.",
+  //         time: "right now",
+  //         userName: "Linsey Listy",
+  //         userColorIndex: 2,
+  //         userImg: person.pictureUrl,
+  //       },
+  //     ]);
+  //   });
+  // }, []);
 
   return (
     <>
-      <div
-        style={{
-          height: "100vh",
-          width: "20%",
-          backgroundColor: "#373737",
-          display: "flex",
-          flexDirection: "column",
-          //   justifyContent: "space-between",
-        }}
-      >
+      <div style={{ display: "flex" }}>
         <div
-          className="LogoAndName"
           style={{
-            // border: "1px solid red",
-            padding: "20px",
-            height: "30%",
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            // alignContent: "center",
-            alignItems: "center",
-            zIndex: 100,
-          }}
-        >
-          <img style={{ width: "50px", height: "50px" }} src={Logo} alt="" />
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              paddingLeft: "15px",
-              //   border: "1px solid",
-            }}
-          >
-            <h2 style={{ color: "white", fontSize: "20px" }}>ASSURANCE +</h2>
-            <h3
-              style={{
-                letterSpacing: "10px",
-                color: "white",
-                fontSize: "10px",
-              }}
-            >
-              INSURANCE
-            </h3>
-          </div>
-        </div>
-        <div
-          className="MenuItems"
-          style={{
-            // border: "1px solid blue",
+            height: "100vh",
+            width: "20%",
+            backgroundColor: "#373737",
             display: "flex",
             flexDirection: "column",
-            padding: "20px",
-            // padding: "20px",
-            height: "90%",
+            //   justifyContent: "space-between",
           }}
         >
-          <Navbar />
+          <div
+            className="LogoAndName"
+            style={{
+              // border: "1px solid red",
+              padding: "20px",
+              height: "30%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              // alignContent: "center",
+              alignItems: "center",
+              zIndex: 100,
+            }}
+          >
+            <img style={{ width: "50px", height: "50px" }} src={Logo} alt="" />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                paddingLeft: "15px",
+                //   border: "1px solid",
+              }}
+            >
+              <h2 style={{ color: "white", fontSize: "20px" }}>ASSURANCE +</h2>
+              <h3
+                style={{
+                  letterSpacing: "10px",
+                  color: "white",
+                  fontSize: "10px",
+                }}
+              >
+                INSURANCE
+              </h3>
+            </div>
+          </div>
+          <div
+            className="MenuItems"
+            style={{
+              // border: "1px solid blue",
+              display: "flex",
+              flexDirection: "column",
+              padding: "20px",
+              // padding: "20px",
+              height: "90%",
+            }}
+          >
+            <Navbar />
+          </div>
+          {/*  */}
         </div>
         {/*  */}
+        <MessageList items={items} />
+        <MessageInput
+          onSubmit={(e: CustomEvent) => {
+            ChatEndpoint.send({});
+            //
+            setItems([
+              ...items,
+              {
+                text: e.detail.value,
+                time: "seconds ago",
+                userName: "Milla Sting",
+                userAbbr: "MS",
+                userColorIndex: 3,
+              },
+            ]);
+          }}
+        />
       </div>
-      <MessageList items={items} />
-      <MessageInput
-        onSubmit={(e) => {
-          setItems([
-            ...items,
-            {
-              text: e.detail.value,
-              time: "seconds ago",
-              userName: "Milla Sting",
-              userAbbr: "MS",
-              userColorIndex: 3,
-            },
-          ]);
-        }}
-      />
     </>
   );
 }
--- src/main/java/com/example/application/ChatEndpoint.java
+++ src/main/java/com/example/application/ChatEndpoint.java
@@ -1,27 +1,34 @@
 package com.example.application;
 
-import java.time.ZonedDateTime;
-
+import java.time.Instant;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-
 import dev.hilla.Endpoint;
-import dev.hilla.runtime.transfertypes.Flux;
 import io.micrometer.common.lang.NonNull;
+import reactor.core.publisher.Sinks;
+import reactor.core.publisher.Sinks.EmitResult;
 import reactor.core.publisher.Sinks.Many;
 
 @Endpoint
 @AnonymousAllowed
 public class ChatEndpoint {
+
     public static class Message {
         public @NonNull String text;
-        public ZonedDateTime time;
+        public Instant time;
         public @NonNull String username;
     }
 
-    private Many<Message> chatsink;
-    private Flux<Message>  chat;
+    private final Many<Message> chatSink = Sinks.many().multicast().directBestEffort();;
+    private final reactor.core.publisher.Flux<Message> chat = chatSink.asFlux().replay(10).autoConnect();;
 
-    ChatEndpoint() {
-        
+    public reactor.core.publisher.Flux<Message> join() {
+        return chat;
     }
+    
+    public void send(Message message) {
+        message.time = Instant.now();
+        chatSink.emitNext(message,
+        (signalType, emitResult) -> emitResult == EmitResult.FAIL_NON_SERIALIZED);
+    }
+
 }
